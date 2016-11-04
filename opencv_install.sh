4032b3e556088691d6928476a184237f37efa396
Clément Mayet <cmayet@gmail.com>
2016-11-04 16:27:08 +0100

    Create opencv_install.sh

---
+++ opencv_install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y --no-install-recommends apt-utils
+apt-get install -y --no-install-recommends python python-pip \
+                                           libpython2.7 \
+                                           build-essential \
+                                           gfortran \
+                                           libatlas-base-dev \
+                                           python-dev \
+                                           python2.7-dev \
+                                           python-tk \
+                                           cmake pkg-config \
+                                           libjpeg8-dev \
+                                           libtiff5-dev \
+                                           libjasper-dev \
+                                           libpng12-dev \
+                                           libavcodec-dev \
+                                           libavformat-dev \
+                                           libswscale-dev \
+                                           libv4l-dev wget \
+                                           unzip
+
+pip install --upgrade pip
+pip install numpy
+mkdir softs 
+cd softs 
+wget --no-check-certificate https://codeload.github.com/opencv/opencv/zip/3.0.0 
+unzip 3.0.0 && rm -rf 3.0.0 
+cd opencv-3.0.0 && mkdir build && cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE  .. 
+make -j 4 
+make install 
+rm -rf /softs
+
+apt-get remove --purge -y gfortran build-essential cmake python-pip
+rm -rf /var/lib/apt/lists/* /var/cache/apt/archives
